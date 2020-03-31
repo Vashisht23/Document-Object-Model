629b1ec570ad91a62d6e9537b90035949b9ce79e
Kunal Sharma <kunalsharms.ks@gmail.com>
2020-03-31 16:49:37 +0530

    Add files via upload

---
+++ coding-challenge.js
@@ -0,0 +1,29 @@
+let getMarksHeight = prompt(); // Add Mark's Height 
+let getMarksWeight = prompt(); // Add Marks's Mass
+
+
+// Store it in Another variable 
+let calMarksBmi = getMarksWeight / (getMarksHeight * getMarksHeight); 
+
+// Round Up the BMI & alert the BMI
+alert(`Mark's Bmi is ` + Math.floor(calMarksBmi));
+
+
+let getJohnsHeight = prompt(); // Add John's  Height
+let getJohnsWeight = prompt(); // Add John's Mass
+
+// Store it in Another variable
+let calJohnsBmi = getJohnsWeight / (getJohnsHeight * getJohnsHeight);
+// Round Up the BMI & alert the BMI
+alert(`John's Bmi is ` + Math.floor(calJohnsBmi));
+
+// Check who's BMI is better
+let checkBmi = calMarksBmi > calJohnsBmi ;
+
+// Alert The Bollean Result
+console.log('So it true that Mark has higher Bmi?' + checkBmi);
+
+
+
+
+
