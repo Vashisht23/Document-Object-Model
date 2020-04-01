841c3faee337938444bc13798ab7d91c4251d17e
Kunal Sharma <kunalsharms.ks@gmail.com>
2020-04-02 01:31:36 +0530

    Update coding-challenge3.js

--- coding-challenge3.js
+++ coding-challenge3.js
@@ -1,9 +1,17 @@
-
+// Store the Amount of Bill in prompt()
 let getBill = prompt();
+
+// Formula for calculating Type 1 Bill
 let createTipType1 = getBill * (20/100); 
 
+// Formula for calculating Type 2 Bill
 let createTipType2 = getBill * (15/100);
+
+// Formula for calculating Type 3 Bill
 let createTipType3 = getBill * (10/100);
+
+
+// Tip: when stuck write comments
 
 function tipCalculator (){
 	
@@ -17,6 +25,7 @@ else if(getBill >= 50 && getBill < 200)
 else if (getBill >= 200 && getBill <=500){
 	alert("Tip to be given is :" +createTipType3 + '$');
 }
+	// Remember No condition to else 
 else {
 	alert("No Tip");
 }
